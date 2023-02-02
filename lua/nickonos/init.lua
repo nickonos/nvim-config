9db1fd31e4e6a803400430e6717ccfe0e3c6dc5d
nickonos <I468166@fhict.nl>
2023-02-02 23:04:49 +0100

    added more setup

--- lua/nickonos/init.lua
+++ lua/nickonos/init.lua
@@ -1,1 +1,2 @@
 require("nickonos.remap")
+require("nickonos.set")
--- lua/nickonos/packer.lua
+++ lua/nickonos/packer.lua
@@ -1,4 +1,4 @@
-  requires = { {'nvim-lua/plenary.nvim'} }
+requires = { {'nvim-lua/plenary.nvim'} }
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
@@ -13,9 +13,45 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   } 
-
-  use { "catppuccin/nvim", as = "catppuccin" }
+    
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  require("rose-pine").setup()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+  use('theprimeagen/harpoon')
+
+  use('mbbill/undotree')
+
+  use('tpope/vim-fugitive')
+
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v1.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},             -- Required
+		  {'williamboman/mason.nvim'},           -- Optional
+		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},         -- Required
+		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
+		  {'hrsh7th/cmp-buffer'},       -- Optional
+		  {'hrsh7th/cmp-path'},         -- Optional
+		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
+		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},             -- Required
+		  {'rafamadriz/friendly-snippets'}, -- Optional
+	  }
+  }
 end)
 
